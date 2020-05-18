3dbc6923ceca168f6eb95013096d71fb1cdb2e6a
lareaza-web <larezaweb@gmail.com>
2020-05-18 15:58:44 -0300

    tipos de datos

---
+++ tipos-de-datos.js
@@ -0,0 +1,29 @@
+'use strict'
+
+// Operadores
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 * numero2;
+
+alert("Es resultado de la operación: " + operacion);
+
+//tipos de datos
+//Integer
+var numero_entero = 44;
+var cadena_texto = 'Hola "que" tal';
+var verdadero_o_falso = false;
+console.log(verdadero_o_falso);
+var numero_falso = "33.4";
+var numero_falso2 = "33";
+
+console.log(Number(numero_falso) + 7);
+//console.log(parseInt(numero_falso2);
+//console.log(parseFloat(numero_falso)+7;
+//console.log(numero_entero+4);
+//console.log(String(numero_entero)+4;
+
+console.log(typeof numero_entero);
+console.log(typeof cadena_texto);
+console.log(typeof numero_falso);
+console.log(typeof numero1);
+console.log(typeof verdadero_o_falso);
